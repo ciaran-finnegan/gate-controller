d7625dbbfd692ed19a8e98fa89c820edc7a6529f
Ciaran Finnegan <ciaran.finnegan@cmd.com.au>
2024-08-29 12:41:19 +0100

    Fix to log time in UTC

--- db_utils.py
+++ db_utils.py
@@ -1,19 +1,15 @@
 import os
 import logging
 import psycopg2
-import datetime
+from datetime import datetime, timezone
 import sqlite3
-
 from logger import configure_logger
 
 # Configure logging
-
 logger = configure_logger()
 
 # Import the upload_image_to_s3 function from s3_utils.py
-
 from s3_utils import upload_image_to_s3
-
 
 # PostgreSQL database configuration
 postgres_url_var = 'POSTGRES_URL'
@@ -25,8 +21,6 @@ postgres_password_var = 'POSTGRES_PASSWORD'
 postgres_database_var = 'POSTGRES_DATABASE'
 
 postgres_url = os.environ.get(postgres_url_var)
-#logger.info(f'postgres_url: {postgres_url}')
-
 postgres_prisma_url = os.environ.get(postgres_prisma_url_var)
 logger.info(f'postgres_prisma_url: {postgres_prisma_url}')
 
@@ -39,13 +33,10 @@ logger.info(f'postgres_user: {postgres_user}')
 postgres_host = os.environ.get(postgres_host_var)
 logger.info(f'postgres_host: {postgres_host}')
 
-# Split the string into parts using '.' as the delimiter and select the first part
 postgres_endpoint = postgres_host.split('.')[0]
 logger.info(f'postgres_endpoint: {postgres_endpoint}')
 
 postgres_password = os.environ.get(postgres_password_var)
-#logger.info(f'postgres_password: {postgres_password}')
-
 postgres_database = os.environ.get(postgres_database_var)
 logger.info(f'postgres_database: {postgres_database}')
 
@@ -56,67 +47,37 @@ postgres_sslmode = 'require'
 logger.info(f'postgres_sslmode: {postgres_sslmode}')
 
 # Specify the full path to the SQLite database directory and filename
-
 db_directory = '/opt/gate-controller/data/'
 db_filename = 'gate-controller-database.db'
 db_file_path = os.path.join(db_directory, db_filename)
 
 # Function to log an entry in the database(s)
 def log_entry(reason,
-               image_path,
-               plate_recognized,
-               score,
-               fuzzy_match,
-               gate_opened,
-               plate_number,
-               vehicle_registered_to_name,
-               vehicle_make,
-               vehicle_model,
-               vehicle_colour):
+              image_path,
+              plate_recognized,
+              score,
+              fuzzy_match,
+              gate_opened,
+              plate_number,
+              vehicle_registered_to_name,
+              vehicle_make,
+              vehicle_model,
+              vehicle_colour):
     # SQLite Entry
     logger.info(f'Logging an entry in the SQLite database log table.')
-    logger.info(f'Calling log_entry_sqlite(reason={reason}, image_path={image_path}, plate_recognized={plate_recognized}, score,fuzzy_match={score}, gate_opened={gate_opened}).')
-    log_entry_sqlite(reason,
-               image_path,
-               plate_recognized,
-               score,
-               fuzzy_match,
-               gate_opened,
-               plate_number,
-               vehicle_registered_to_name,
-               vehicle_make,
-               vehicle_model,
-               vehicle_colour)
+    log_entry_sqlite(reason, image_path, plate_recognized, score, fuzzy_match, gate_opened, 
+                     plate_number, vehicle_registered_to_name, vehicle_make, vehicle_model, vehicle_colour)
 
-    
     # PostgreSQL Entry
     logger.info(f'Logging an entry in the PostgreSQL database log table.')
-    logger.info(f'Calling log_entry_postgres(reason={reason}, image_path={image_path}, plate_recognized={plate_recognized}, score,fuzzy_match={score}, gate_opened={gate_opened}).')
-    
-    # First we need to upload the image to S3 and get the object URL
     image_url = upload_image_to_s3(image_path)
+    image_path = image_url  # Replace the local image path with the S3 object URL
 
-    # Then we can replace the local image_path with the S3 object URL
-
-    image_path = image_url
-
-    # Now we can log the entry in the PostgreSQL database
-
-    log_entry_postgres(reason,
-               image_path,
-               plate_recognized,
-               score,
-               fuzzy_match,
-               gate_opened,
-               plate_number,
-               vehicle_registered_to_name,
-               vehicle_make,
-               vehicle_model,
-               vehicle_colour)
+    log_entry_postgres(reason, image_path, plate_recognized, score, fuzzy_match, gate_opened, 
+                       plate_number, vehicle_registered_to_name, vehicle_make, vehicle_model, vehicle_colour)
 
 # Function to create the SQLite database table if it doesn't exist
 def create_table_sqlite():
-    # Log the db_file_path
     logger.info(f'SQLite db_file_path: {db_file_path}')
 
     # Ensure the database directory exists
@@ -124,13 +85,8 @@ def create_table_sqlite():
         os.makedirs(db_directory)
 
     try:
-        # Connect to the database and create the "log" table if it doesn't exist
         conn = sqlite3.connect(db_file_path)
         cursor = conn.cursor()
-
-        # Create the "log" table if it doesn't exist
-        # No boolean types in SQLite, fuzzy_match and gate_opened are stored as TEXT
-        
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS log (
                 id INTEGER PRIMARY KEY,
@@ -148,10 +104,8 @@ def create_table_sqlite():
                 vehicle_colour TEXT
             )
         ''')
-
         conn.commit()
         conn.close()
-
         logger.info(f'Database table created successfully.')
     except sqlite3.Error as sql_error:
         logger.error(f'SQLite error while creating the table: {str(sql_error)}')
@@ -162,8 +116,6 @@ def create_table_sqlite():
 def create_table_postgres(conn):
     try:
         cursor = conn.cursor()
-        
-        # Create the "log" table if it doesn't exist
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS log (
                 id SERIAL PRIMARY KEY,
@@ -187,20 +139,21 @@ def create_table_postgres(conn):
         logger.error(f'PostgreSQL error while creating/checking the table: {str(sql_error)}')
     except Exception as e:
         logger.error(f'Error creating/checking the table in PostgreSQL: {str(e)}')
-    
+
 # Function to log an entry in the local SQLite database
 def log_entry_sqlite(reason,
-               image_path,
-               plate_recognized,
-               score,
-               fuzzy_match,
-               gate_opened,
-               plate_number,
-               vehicle_registered_to_name,
-               vehicle_make,
-               vehicle_model,
-               vehicle_colour):
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                     image_path,
+                     plate_recognized,
+                     score,
+                     fuzzy_match,
+                     gate_opened,
+                     plate_number,
+                     vehicle_registered_to_name,
+                     vehicle_make,
+                     vehicle_model,
+                     vehicle_colour):
+    # Use UTC time for timestamp
+    current_time = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
     try:
         conn = sqlite3.connect(db_file_path)
         cursor = conn.cursor()
@@ -213,11 +166,9 @@ def log_entry_sqlite(reason,
     except Exception as e:
         logger.error(f'Error while logging an entry in SQLite: {str(e)}')
     finally:
-        # Ensuring that the connection is closed even if an error occurs
         conn.close()
 
 # Function to log an entry in the remote PostgreSQL database
-
 def log_entry_postgres(reason,
                        image_path,
                        plate_recognized,
@@ -229,17 +180,16 @@ def log_entry_postgres(reason,
                        vehicle_make,
                        vehicle_model,
                        vehicle_colour):
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # Use UTC time for timestamp
+    current_time = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
     try:
         conn_str = f"dbname={postgres_database} user={postgres_user} password={postgres_password} host={postgres_host} port={postgres_port} sslmode={postgres_sslmode} options=endpoint={postgres_endpoint}"
-        conn = psycopg2.connect(conn_str)  
-        
+        conn = psycopg2.connect(conn_str)
+
         # Ensure the log table exists
         create_table_postgres(conn)
-        
+
         cursor = conn.cursor()
-        
-        # Execute the INSERT query
         cursor.execute('''
         INSERT INTO log (
             timestamp, reason, image_path, plate_recognized, score,
@@ -249,7 +199,7 @@ def log_entry_postgres(reason,
     ''', (current_time, reason, image_path, plate_recognized, score,
           fuzzy_match, gate_opened, plate_number,
           vehicle_registered_to_name, vehicle_make, vehicle_model, vehicle_colour))
-        
+
         conn.commit()
         logger.info(f'Logged an entry in the PostgreSQL database for plate: {plate_number}')
     except psycopg2.Error as sql_error:
